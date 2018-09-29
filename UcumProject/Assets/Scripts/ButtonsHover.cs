e400655589ae7968d6622eeefe0b32562cc86594
Ashot-PC\Ashot <ashot11997@mail.ru>
2018-09-29 21:47:57 +0500

    Finished Header and Menu Slider

---
+++ UcumProject/Assets/Scripts/ButtonsHover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonsHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	public Image BtnImage;
+	public Image ActiveLine;
+	public Text BtnText;
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		BtnImage.color = new Color32(26,26,26,140);
+		BtnText.color = new Color32(110, 169, 0, 255);
+		ActiveLine.gameObject.SetActive(true);
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		BtnImage.color = new Color32(26, 26, 26, 120);
+		BtnText.color = new Color32(255, 255, 255, 255);
+		ActiveLine.gameObject.SetActive(false);
+	}
+}
---
+++ UcumProject/Assets/Scripts/HeaderUIController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+public class HeaderUIController : MonoBehaviour {
+
+	public Text TimeText;
+
+	private float m_timer = 0;
+	private float m_timeToUpdateTime = 60f;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (m_timer > 0)
+		{
+			m_timer -= Time.deltaTime;
+		}
+		else
+		{
+			m_timer = m_timeToUpdateTime;
+			SetTime();
+		}
+	}
+
+	private void SetTime()
+	{
+		TimeText.text = DateTime.Now.ToShortTimeString();
+	}
+}
